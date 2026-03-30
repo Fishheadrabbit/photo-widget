1c467b528a7a4e25f7a3e904dab527091c0f3daf
Fishheadrabbit <105645188+Fishheadrabbit@users.noreply.github.com>
2026-03-30 22:32:39 +0800

    更新 build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -137,7 +137,7 @@ androidComponents {
         )
 
         variant.androidResources.localeFilters
-            .addAll("en", "de", "es", "fr", "it", "iw", "ja", "pt", "ro", "ru", "fil", "tr")
+            .addAll("en", "de", "es", "fr", "it", "iw", "ja", "pt", "ro", "ru", "fil", "tr", "zh")
     }
 }
 
